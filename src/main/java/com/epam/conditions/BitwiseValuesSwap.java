facdbec89cf2c5ca578fb6692d9c9c5d8f68cd42
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-19 17:38:12 +0300

    Update BitwiseValuesSwap.java

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -1,8 +1,13 @@
 package com.epam.conditions;
 
 public class BitwiseValuesSwap {
-
     public void swap(int first, int second) {
-    }
+        
+        int temp = first;
+        first = second;
+        second = temp;
 
+        System.out.println(first);
+        System.out.println(second);
+     }
 }
