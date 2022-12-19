f9997e2d850ba64943c3dbb943aff8d352934226
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-19 17:44:49 +0300

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,28 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
-
+    int rowCount = 1;
+        for (int i = cathetusLength; i > 0; i--)
+        {
+            for (int j = 2; j <= i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = rowCount; j >= 1; j--)
+            {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= rowCount; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+            rowCount++;
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
