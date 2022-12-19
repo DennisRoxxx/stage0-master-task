dfbda233257ad69d81195bc03c08cd58b60bc2c7
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-19 17:44:27 +0300

    Update Power.java

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -1,13 +1,21 @@
 package com.epam.loops;
 
 public class Power {
-
     public void printPower(int numberToPrint, int power) {
+        int sum = 0;
 
+        for (int i = 1; i <= 1; i++){
+            sum = numberToPrint;
+        }
+
+        for (int i = 2; i <= power; i++){
+            sum = sum * numberToPrint;
+        }
+        System.out.println(sum);
+        
     }
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
     }
-
 }
