1be1eab464259f53a1284eba5224ebcb12678e3b
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-19 17:42:49 +0300

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,15 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
     public void revert(int number) {
-    }
-
+        
+        int reversedNumber = 0;
+        if (number > 99) {
+            while (number != 0) {
+                reversedNumber = reversedNumber * 10 + number % 10;
+                number /= 10;
+            }
+            System.out.println(reversedNumber);
+        }
+   }
 }
